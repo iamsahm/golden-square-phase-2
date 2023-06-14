3f3adfa0ba3cbbdf8b87bd7a05804a8065eaa21b
Bradley <brad.holmes05@icloud.com>
2023-06-14 16:14:11 +0100

    upload challeneg

--- mocking_bites/01_unit_testing/challenge/tests/test_task_list.py
+++ mocking_bites/01_unit_testing/challenge/tests/test_task_list.py
@@ -1,4 +1,5 @@
 from lib.task_list import TaskList
+from unittest.mock import Mock
 
 
 def test_task_list_initially_empty():
@@ -9,5 +10,46 @@ def test_task_list_initially_empty():
 def test_tasks_initially_not_all_complete():
     task_list = TaskList()
     assert task_list.all_complete() == False
+
+def test_task_list_add():
+    task_list = TaskList()
+
+    fake_task = Mock()
+
+    task_list.add(fake_task)
+
+    assert fake_task in task_list.tasks
+
+def test_all_method():
+    task_list = TaskList()
+
+    fake_task_1 = Mock()
+    fake_task_2 = Mock()
+
+    task_list.add(fake_task_1)
+    task_list.add(fake_task_2)
+
+    all_tasks = task_list.all()
+    assert all(item in all_tasks for item in [fake_task_1, fake_task_2])
+
+def test_task_all_complete():
+
+    task_list = TaskList()
+
+    fake_task_1 = Mock()
+    fake_task_2 = Mock()
+
+    task_list.add(fake_task_1)
+    task_list.add(fake_task_2)
+
+    fake_task_1.is_complete.return_value = True
+    fake_task_2.is_complete.return_value = True
+
+    assert task_list.all_complete() == True
+
+
+    
+    
+
 
 # Unit test `#tasks` and `#all_complete` behaviour
